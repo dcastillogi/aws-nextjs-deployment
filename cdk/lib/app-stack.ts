d512a3b87826e325bc3b82c21954c0f1170a55b7
Daniel Castillo <dev@dcastillogi.com>
2024-12-15 21:51:29 -0500

    Caching update

--- cdk/lib/app-stack.ts
+++ cdk/lib/app-stack.ts
@@ -298,9 +298,9 @@ export class AppStack extends cdk.Stack {
             {
                 cachePolicyName: "CustomALBCachePolicy",
                 comment: "Cache policy for ALB origin with Host header",
-                defaultTtl: Duration.days(1),
+                defaultTtl: Duration.days(0),
                 minTtl: Duration.seconds(0),
-                maxTtl: Duration.days(365),
+                maxTtl: Duration.days(0),
                 headerBehavior:
                     cloudfront.CacheHeaderBehavior.allowList("Host"),
                 enableAcceptEncodingGzip: true,
@@ -377,7 +377,7 @@ export class AppStack extends cdk.Stack {
                         cloudfront.OriginRequestPolicy.ALL_VIEWER,
                 },
                 additionalBehaviors: {
-                    "static/*": {
+                    "/static/*": {
                         origin: origins.S3BucketOrigin.withOriginAccessControl(
                             staticBucket
                         ),
